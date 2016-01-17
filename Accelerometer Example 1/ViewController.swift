4feee3ba614eca9b831436f8aea08478eaf4e268
ibarra2521 <ibarra2521@gmail.com>
2016-01-17 08:39:11 -0500

    First version

--- Accelerometer Example 1/ViewController.swift
+++ Accelerometer Example 1/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblX: UILabel!
+    @IBOutlet weak var lblY: UILabel!
+    @IBOutlet weak var lblZ: UILabel!
+    @IBOutlet weak var lblShake: UILabel!
+    
+    private let manager = CMMotionManager()
+    private let queue = NSOperationQueue()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if manager.accelerometerAvailable {
+            manager.accelerometerUpdateInterval = 1.0 / 10.0
+            manager.startAccelerometerUpdatesToQueue(queue, withHandler: {
+                data, error in
+                if error != nil {
+                    self.manager.stopAccelerometerUpdates()
+                }else {
+                    dispatch_async(dispatch_get_main_queue(), {
+                        self.lblX.text = "\(data!.acceleration.x)"
+                        self.lblY.text = "\(data!.acceleration.y)"
+                        self.lblZ.text = "\(data!.acceleration.z)"
+                        
+                        if (data!.acceleration.x > 1.6) ||
+                            (data!.acceleration.y > 1.6) ||
+                            (data!.acceleration.z > 1.6) {
+                            self.lblShake.text = "Yes!!!"
+                        }
+                    })
+                }
+            })
+        }else {
+            print("Accelerometer Unavailable")
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func clean() {
+        self.lblShake.text = ""
     }
-
 
 }
 
